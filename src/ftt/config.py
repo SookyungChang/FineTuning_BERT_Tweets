3a92f2b10d6812f14b5996b9ac32f8f063fcfc5d
sweetguma <sooensible@gmail.com>
2026-09-23 06:49:22 +0200

    Config: seperate VERSION and F1_AVG

--- src/ftt/config.py
+++ src/ftt/config.py
@@ -4,31 +4,32 @@ from pathlib import Path
 @dataclass
 class Config():
      SEED: int = 42
-     VERSION: str = "1.0.0"
-     F1_AVG: str = "macro"
 
 @dataclass
 class pathConfig:
     PROJECT_ROOT: Path = Path(__file__).resolve().parents[2]
     DATA_DIR: Path = PROJECT_ROOT / "data"
     EXP_DIR: Path = PROJECT_ROOT / "saved_parameters"
-    SAVE_MODEL_PATH: Path = PROJECT_ROOT / "saved_models"
+    SAVED_MODELS_PATH: Path = PROJECT_ROOT / "saved_models"
 
 @dataclass
 class bertConfig():
-        MODEL_NAME: str = "distilbert-base-uncased-finetuned-sst-2-english"
-        
-        NUM_EPOCHS: int = 1
-        DROUP_OUT: float = 0.1
-        FINED_TUNED_MODEL_NAME: str = field(init=False)
-        def __post_init__(self):
-            self.FINED_TUNED_MODEL_NAME = f"sweetguma/bert-sentiment-model-v{self.version}"
+    HF_MODEL_NAME: str = "distilbert-base-uncased-finetuned-sst-2-english"
+    VERSION: str = "1.0.0"
+    NUM_EPOCHS: int = 1
+    F1_AVG: str = "macro"
+    DROUP_OUT: float = 0.1
+    FINED_TUNED_MODEL_NAME: str = field(init=False)
+    def __post_init__(self):
+        self.FINED_TUNED_MODEL_NAME = f"sweetguma/bert-sentiment-model-v{self.VERSION}"
     
 @dataclass
 class baseConfig():
     # hyper parameter searching OPNUNA?
     N_TRIALS: int = 150
     SAMPLE_SIZE: int = 100000
+    VERSION: str = "1.0.0"
+    F1_AVG: str = "macro"
 
     # Searching space?
     MIN_DF: tuple = (1, 100)
