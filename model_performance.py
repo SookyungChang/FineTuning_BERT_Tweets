da83978556882ba53d536372b717dae84df8d246
sweetguma <sooensible@gmail.com>
2026-09-23 09:40:18 +0200

    Feat: check the performace of models

---
+++ model_performance.py
@@ -0,0 +1,43 @@
+from datasets import load_from_disk
+from sklearn.metrics import f1_score
+
+from ftt.inference.predictor_base import load_model
+from ftt.inference.predictor_bert import Predictor
+from ftt.config import baseConfig, pathConfig, bertConfig
+
+paths = pathConfig()
+bases = baseConfig()
+berts = bertConfig()
+
+def compare_performance():
+
+    dataset = load_from_disk(
+        paths.DATA_DIR / "tweets_dataset"
+    )
+
+    X_test = dataset["test"]["text"]
+    y_test = dataset["test"]["label"]
+
+    base = load_model()
+    base_preds = base.predict(X_test)
+
+    base_f1 = f1_score(
+        y_test,
+        base_preds,
+        average=bases.F1_AVG,
+    )
+
+    bert = Predictor()
+    bert_preds = bert.predict(X_test)
+
+    bert_f1 = f1_score(
+            y_test,
+            bert_preds,
+            average=berts.F1_AVG,
+        )
+
+
+    print(f"Test F1 score: Baseline {base_f1:.4f} | BERT {bert_f1:.4f}")
+
+if __name__ == "__main__":
+    compare_performance()
